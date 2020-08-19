35f51ea67fdee4f206df115c9666726ab4740bcb
Carlos E. Alvarez-Berrios <calvarezberrios@Carloss-MacBook-Pro.local>
2020-08-19 03:25:32 -0400

    Added entries table to migrations

--- data/migrations/20200816105210_create-tables.js
+++ data/migrations/20200816105210_create-tables.js
@@ -9,9 +9,25 @@ exports.up = function(knex) {
                 tbl.varchar("username", 128).notNullable().unique();
                 tbl.varchar("password", 128).notNullable();
             })
+            .createTable("entries", tbl => {
+                tbl.increments();
+                tbl.datetime("sleep_start").notNullable().unique();
+                tbl.datetime("sleep_end").unique();
+                tbl.integer("daytime_mood");
+                tbl.integer("sleep_start_mood");
+                tbl.integer("sleep_end_mood");
+                tbl.time("sleep_time_total");
+                tbl.integer("user_id")
+                    .unsigned()
+                    .notNullable()
+                    .references("users.id")
+                    .onUpdate("CASCADE")
+                    .onDelete("CASCADE");
+            })
 };
 
 exports.down = function(knex) {
     return knex.schema
+            .dropTableIfExists("entries")
             .dropTableIfExists("users");
 };
