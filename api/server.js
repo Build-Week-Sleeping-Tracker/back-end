58a386017ef9d688c94094d48ab9a32fd0c5a4ae
Carlos E. Alvarez-Berrios <calvarezberrios@Carloss-MacBook-Pro.local>
2020-08-20 13:04:03 -0400

    Added public folder for static html pages, created home page, about page, and documentation page, no styles set up yet

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("helmet");
 const morgan = process.env.NODE_ENV !== "production" ? require("morgan") : null;
+const path = require("path");
 
 const authenticate = require("../auth/authenticate.js");
 const authRouter = require("../auth/authRouter.js");
@@ -21,7 +22,7 @@ server.use("/api/auth", authRouter);
 server.use("/api/users", authenticate, usersRouter);
 server.use("/api/sleep", authenticate, entriesRouter);
 
-server.get("/", (req, res) => res.json({api: "up"}));
+server.use(express.static("public"));
 
 server.use((req, res, next) => next({ code: 404, message: `The ${req.method} method for ${req.url} does not exist. Check the url and try again` }));
 
